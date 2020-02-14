6f40f8279a573db1300401be76b99b49f8bf9737
Student <example@example.com>
2020-02-14 08:49:13 -0800

    set up App.js page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Ale from './components/Ale';
+import AleAdd from './components/AleAdd';
+import AleList from './components/AleList';
+import AleEdit from './components/AleEdit';
+import ReviewAdd from './components/ReviewAdd';
+import ReviewList from './components/ReviewList';
+import ReviewEdit from './components/ReviewEdit';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-        </p>
-      </header>
-    </div>
+  <Header />
+  <Nav />
+  <Switch>
+    <Route exact path='/' component={Ale} />
+    <Route path='/aleadd' component={AleAdd} />
+    <Route path='/alelist' component={AleList} />
+    <Route path='/aleedit' component={AleEdit} />
+    <Route path='/reviewadd' component={ReviewAdd} />
+    <Route path='/reviewlist' component={ReviewList} />
+    <Route path='/reviewedit' component={ReviewEdit} />
+  </Switch>
+  </div>
   );
 }
 
