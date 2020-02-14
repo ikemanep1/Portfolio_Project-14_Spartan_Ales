4537898352cadc3360e8dc68f9fc7be86947411e
Student <example@example.com>
2020-02-14 08:59:41 -0800

    add content to Ale.jsx page

---
+++ src/components/Ale.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Ale(props){
+  return (
+    <div className="AleSingular">
+    <p>{props.name}</p>
+    <p>{props.type}</p>
+    <p>{props.process}</p>
+    <p>{props.alcohol_content}</p>
+  </div>
+   );
+}
+
+Ale.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  process: PropTypes.string,
+  alcohol_content: PropTypes.string
+};
+
+
+export default Ale;
