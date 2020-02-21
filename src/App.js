410093e381832980625bd7152967ba109348edba
ikemanep1 <ike.esquivelpilloud@gmail.com>
2020-02-21 10:23:38 -0800

    convert app component into stateful class component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,41 @@ import Review from './components/Review';
 import ReviewAdd from './components/ReviewAdd';
 import ReviewList from './components/ReviewList';
 import Error404 from './components/Error404';
-import { Switch, Route } from 'react-router-dom';
-
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Moment from 'moment';
+import {Link, withRouter} from "react-router-dom";
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterAleList: [],
+      masterReviewList: []
+    };
+    this.handleAddingNewAleToList = this.handleAddingNewAleToList.bind(this);
+    this.handleAddingNewReviewToList = this.handleAddingNewReviewToList.bind(this);
+
+  }
+
+  handleAddingNewAleToList(newAle) {
+    let newMasterAleList = this.state.masterAleList.slice();
+    newAle.formattedSinceBirth = (newAle.sinceBirth).fromNow(true);
+
+    newMasterAleList.push(newAle);
+    this.setState({masterAleList: newMasterAleList});
+  }
+
+  handleAddingNewReviewToList(newReview) {
+    let newMasterReviewList = this.state.masterReviewList.slice();
+    newReview.formattedSinceBirth = (newReview.sinceBirth).fromNow(true);
+
+    newMasterReviewList.push(newReview);
+    this.setState({masterReviewList: newMasterReviewList});
+  }
+
+  render() {
   const routeStyles = {
     width: "80%",
     backgroundColor: "#660000",
@@ -24,23 +54,32 @@ function App() {
     textAlign: "center",
     color: "#ffedcc",
   }
-  return (
-
-    <div className="App">
+  return (<div style={routeStyles} className="App">
     <header className="App-header">
     <Nav/>
     </header>
     <div>
+    <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={AleList} />
-      <Route path='/aleadd' component={AleAdd} />
-      <Route path='/reviewadd' component={ReviewAdd} />
-      <Route path='/reviewlist' component={ReviewList} />
+      <Route exact path='/' render={() =>< AleList aleList = {
+          this.state.masterAleList
+        } />}/>
+      <Route path='/aleadd' render={() =>< AleAdd onAleCreation = {
+          this.handleAddingNewAleToList
+        } />}/>
+      <Route path='/reviewadd' render={() =>< ReviewAdd onReviewCreation = {
+          this.handleAddingNewReviewToList
+        } />}/>
+      <Route path='/reviewlist' render={() =>< ReviewList reviewList = {
+          this.state.masterReviewList
+        } />}/>
       <Route component={Error404} />
     </Switch>
+    </BrowserRouter>
     </div>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/AleAdd.jsx
+++ src/components/AleAdd.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { v4 } from 'uuid';
 
 function AleAdd(){
   const aleFormStyles = {
@@ -17,7 +18,7 @@ function AleAdd(){
   }
   return (
     <div style={aleFormStyles}>
-    <form>
+    <form onSubmit={handleNewAleSubmission}>
     <input
     type='text'
     id='name'
@@ -53,3 +54,49 @@ function AleAdd(){
 }
 
 export default AleAdd;
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { v4 } from 'uuid';
+//
+// function NewTicketForm(props) {
+//   let _names = null;
+//   let _location = null;
+//   let _issue = null;
+//
+//   function handleNewTicketFormSubmission(event) {
+//     event.preventDefault();
+//     props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4()});
+//     _names.value = '';
+//     _location.value = '';
+//     _issue.value = '';
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleNewTicketFormSubmission}>
+//         <input
+//         type='text'
+//         id='names'
+//         placeholder='Pair Names'
+//         ref={(input) => {_names = input;}}/>
+//         <input
+//         type='text'
+//         id='location'
+//         placeholder='Location'
+//         ref={(input) => {_location = input;}}/>
+//         <textarea
+//         id='issue'
+//         placeholder='Describe your issue.'
+//         ref={(textarea) => {_issue = textarea;}}/>
+//         <button type='submit'>Help!</button>
+//       </form>
+//     </div>
+//   );
+// }
+//
+// NewTicketForm.propTypes = {
+//   onNewTicketCreation: PropTypes.func
+// };
+//
+// export default NewTicketForm;
